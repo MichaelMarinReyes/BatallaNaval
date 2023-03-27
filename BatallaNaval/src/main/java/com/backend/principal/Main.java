b426104666e6d752bd7712f39daad791c97bc89c
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2023-03-27 01:10:06 -0600

    Creación de interfaz de usuario básica

---
+++ BatallaNaval/src/main/java/com/backend/principal/Main.java
@@ -0,0 +1,15 @@
+package com.backend.principal;
+
+import com.frontend.ventanaprincipal.Principal;
+
+/**
+ *
+ * @author michael
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+    }
+}
