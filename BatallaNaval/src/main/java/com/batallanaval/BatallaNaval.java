7188b7c2871ff3634e87a00e0dac0b53fdc763f4
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2023-03-26 22:50:43 -0600

    Creación práctica

---
+++ BatallaNaval/src/main/java/com/batallanaval/BatallaNaval.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.batallanaval;
+
+/**
+ *
+ * @author michael
+ */
+public class BatallaNaval {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
